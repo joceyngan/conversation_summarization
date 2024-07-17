04f9153812445d9b36eb2280a6109e2f9f327c1c
Jocelyn <jocey.ngan@gmail.com>
2024-07-17 12:52:19 +0800

    update finetuned model link on HF and use by default

--- model.py
+++ model.py
@@ -1,16 +1,15 @@
 import torch
 from pathlib import Path
 from transformers import BartTokenizer, BartForConditionalGeneration
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM\
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# Using original pre-trained facebook/bart-large-cnn
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+# Original pre-trained facebook/bart-large-cnn
+# tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+# model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
 
 # Using self finetuned bart-large-cnn on samsum dataset
-# checkpoint_dir = "<path to model>"
-# tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-# model = BartForConditionalGeneration.from_pretrained(checkpoint_dir)
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+model = BartForConditionalGeneration.from_pretrained("jngan/bart-large-cnn-samsum")
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
